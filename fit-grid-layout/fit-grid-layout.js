6660013e577c12f18b3023081e2cc71f51652c07
thotf83 <molnar.laszlo.2k@gmail.com>
2026-06-27 06:49:04 +0200

    feat: add FitGridLayout custom component with dynamic scaling and popup support

--- fit-grid-layout/fit-grid-layout.js
+++ fit-grid-layout/fit-grid-layout.js
@@ -311,6 +311,7 @@ class FitGridLayout extends HAControlBase {
   }
 
   render() {
+    console.log("[FitGridLayout] RENDER: _activePopup:", this._activePopup, "has popupEl:", !!this._popupEl);
     if (!this.config || !this.hass) return html``;
 
     const layout = this.config.layout || {};
